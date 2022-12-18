c95f492a94bec50678a2ff77fcfaf9768ccc8173
unknown <damian1234567812@gmail.com>
2022-12-18 19:24:42 +0100

    added content in Info Tab

--- src/main/java/org/openjfx/DashboardController.java
+++ src/main/java/org/openjfx/DashboardController.java
@@ -1,5 +1,6 @@
 package org.openjfx;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -10,6 +11,11 @@ import game.management.CurrentlyPlayedGame;
 import game.management.ManageGame;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import org.w3c.dom.Text;
 
 public class DashboardController {
 
@@ -21,6 +27,16 @@ public class DashboardController {
 
     @FXML
     Label dashboardTabInfoIsReady;
+    @FXML
+    ImageView dashboardTabInfoLogo;
+    @FXML
+    Label dashboardTabInfoLogoLabel;
+    @FXML
+    TextArea dashboardTabInfoCompanyName;
+    @FXML
+    TextArea dashboardTabInfoCompanyMission;
+
+
 
     @FXML
     private void initialize() {
@@ -37,6 +53,26 @@ public class DashboardController {
     @FXML
     private void switchToAccount() throws IOException {
         App.setRoot("useraccountscreen");
+    }
+
+    /**
+     * Metoda która pozwala na wybór pliku i wybranie go jako logo
+     */
+    @FXML
+    private void fileSelection(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Logo for your Company");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*jpg"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+        File selectedFileInput = selectedFile;
+
+        if(selectedFile != null) {
+            dashboardTabInfoLogoLabel.setText("Zostało Wybrane Logo:");
+            Image image = new Image(selectedFile.toURI().toString(), 240, 240, true, true,true);
+            dashboardTabInfoLogo.setImage(image);
+        } else {
+            dashboardTabInfoLogoLabel.setText("Wybierz Logo Firmy");
+        }
     }
 
     /**
